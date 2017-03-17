4067bc4448ae9690875183e382769b36abc3d513
Krinitsyn Anton <Kr1an@hotmail.com>
2017-03-17 20:54:21 +0300

    remove-more/feature: empty property_reader unittests

---
+++ tests/test_property_reader.py
@@ -0,0 +1,10 @@
+import unittest
+from utils.helpers import property_reader
+
+
+class PropertyReaderTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_to_test_unittest(self):
+        self.assertEqual(True, True, msg="Placeholder test msg")
