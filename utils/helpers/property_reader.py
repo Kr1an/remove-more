7e4a52a4c4582d6135cd98e13039ef318a9c96c0
Krinitsyn Anton <Kr1an@hotmail.com>
2017-03-17 20:55:34 +0300

    remove-more/feature: add property_reader

---
+++ utils/helpers/property_reader.py
@@ -0,0 +1,76 @@
+"""Package to search in obj, json for properties
+
+Package is needed to search such query: 'property1.property2.property3'
+with in json or python dictionary.
+
+"""
+
+import json
+
+
+def get_property(obj, search_query, sep='.'):
+    """Get Property function
+
+    Function's needed to find properties with search query.
+
+    Args:
+        obj:
+            Object in which, needed property lays.
+            Example: {'name': 'some_name', 'age': 'some_age'}.
+        search_query:
+            Chain of property that needed to be find as a string separated
+            by sep(default: '.').
+            Example: property1.property2.property3...
+        sep:
+            Character, that separate properties in property chain.
+            Default: '.'(point)
+
+    Returns:
+        None: returns 'None' if property does not exists.
+        Value: returns 'Value' if property does exists.
+
+    """
+
+    container = obj
+    targets = search_query.split(sep)
+
+    for target in targets:
+        if isinstance(container, dict) and target in container:
+            container = container[target]
+        else:
+            return None
+
+    return container
+
+
+def get_property_from_json(json_str, search_query, sep='.'):
+    """Get Property function
+
+        Function's needed to find properties
+        with search query within json.
+
+        Args:
+            json_str:
+                JSON obj in which, needed property lays.
+                Example: '{"name": "some_name", "age": "some_age"}'.
+            search_query:
+                Chain of property that needed to be find as a string separated
+                by sep(default: '.').
+                Example: 'property1.property2.property3'
+            sep:
+                Character, that separate properties in property chain.
+                Default: '.'(point)
+
+        Returns:
+            None: returns 'None' if property does not exists.
+            Value: returns 'Value' if property does exists.
+
+    """
+
+    obj = json.loads(json_str)
+    return get_property(obj, search_query, sep)
+
+
+
+
+
