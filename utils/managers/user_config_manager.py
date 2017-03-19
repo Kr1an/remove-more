fe45f8b4f5f5c5ba8292de3aa17cb6d2b33bc875
Krinitsyn Anton <Kr1an@hotmail.com>
2017-03-19 16:57:41 +0300

    remove-more/feature: refactor config managers to work with json configs

--- utils/managers/user_config_manager.py
+++ utils/managers/user_config_manager.py
@@ -1,19 +1,19 @@
-import os
 import json
 
 from utils.helpers import property_reader, property_writer
+from utils.managers import app_config_manager
 from setting import DEFAULT_CONFIGS
 
-APP_CONFIG_FILE_PATH = \
-    os.path.abspath(
-        os.path.join(
-            __file__,
-            '..',
-            '..',
-            '..',
-            'setting',
-            'app_config.json')
+
+def _get_config_path():
+    use_custom_config = app_config_manager.get_property(
+        'user_config.use_custom'
     )
+    search_query = 'user_config.path.{}'.format(
+        'custom' if use_custom_config else 'default'
+    )
+
+    return app_config_manager.get_property(search_query)
 
 
 def get_property(search_query):
@@ -23,31 +23,16 @@ def get_property(search_query):
 
 def initialize():
     _set_default_config()
-    _set_properties([
-        {
-            'key': 'user_config.use_custom',
-            'value': False
-        },
-        {
-            'key': 'user_config.path.default',
-            'value': os.path.join(
-                os.path.dirname(APP_CONFIG_FILE_PATH),
-                'user_config_default.json'
-            )
-        },
-        {
-            'key': 'user_config.path.custom',
-            'value': os.path.join(
-                os.path.dirname(APP_CONFIG_FILE_PATH),
-                'user_config_custom.json'
-            )
-        }
-    ])
+    _set_properties([])
 
 
 def _set_config(config):
     try:
-        json_app_config_file = open(APP_CONFIG_FILE_PATH, 'w')
+        config_path = _get_config_path()
+        if config_path is None:
+            return None
+
+        json_app_config_file = open(config_path, 'w')
         json_app_config_file.write(json.dumps(config, indent=4))
         return True
     except Exception as e:
@@ -57,7 +42,11 @@ def _set_config(config):
 
 def _get_config():
     try:
-        json_app_config_file = open(APP_CONFIG_FILE_PATH, 'r')
+        config_path = _get_config_path()
+        if config_path is None:
+            return None
+
+        json_app_config_file = open(config_path, 'r')
         config = json.load(json_app_config_file)
         return config
     except Exception as e:
@@ -70,15 +59,16 @@ def set_property(search_query, value):
     config = property_writer.set_property(config, search_query, value)
 
     if config is None:
-        return None
+        return False
 
     return _set_config(config)
 
 
 def _set_default_config():
-    _set_config(DEFAULT_CONFIGS.APP_CONFIG)
+    _set_config(DEFAULT_CONFIGS.USER_CONFIG)
 
 
 def _set_properties(props):
     for prop in props:
         set_property(prop['key'], prop['value'])
+
