54ed215ae215b25ce639d6eb2d148b5a8a34fb82
Krinitsyn Anton <Kr1an@hotmail.com>
2017-03-19 16:03:28 +0300

    remove-more/feature: move json setting files to gitignore, add default_configs.py

---
+++ setting/DEFAULT_CONFIGS.py
@@ -0,0 +1,24 @@
+APP_CONFIG = {
+    "supported_platforms": [
+        "linux",
+        "linux2"
+    ],
+    "version": "",
+    "user_config": {
+        "path": {
+            "default": "",
+            "custom": ""
+        },
+        "use_custom": False
+    },
+    "app_location": "",
+    "author": "kr1an@hotmail.com"
+}
+
+USER_CONFIG = {
+    "name": {
+        "short": "rr",
+        "normal": "RemoveRestore",
+    }
+}
+
