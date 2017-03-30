988737f2d271245b8d81e8feea4c3aefffa866d9
Krinitsyn Anton <Kr1an@hotmail.com>
2017-03-30 19:40:36 +0300

    clean-path-module/feature: add docstring to module

--- utils/helpers/clean_path.py
+++ utils/helpers/clean_path.py
@@ -16,6 +16,19 @@ import shutil
 
 
 def delete(path, options=None):
+    """Delete Function
+    
+    Function allow deleting object by path. Work similar with file/dir/link.
+    If dir, remove recursively.
+    
+    Parameters:
+        path: object location.
+        options: list of deleting politics.
+    
+    Returns:
+        value: 0 - successful, 1 - fail.
+    
+    """
     try:
         if os.path.isdir(path):
             shutil.rmtree(path)
@@ -28,6 +41,20 @@ def delete(path, options=None):
 
 
 def copy(src, dest, options=None):
+    """Copy Function
+
+        Function allow to copy object by path. 
+        Work similar with file/dir/link. If dir, copy recursively.
+
+        Parameters:
+            src: object location.
+            dest: object copying path.
+            options: list of copying politics.
+
+        Returns:
+            value: 0 - successful, 1 - fail.
+
+        """
     try:
         if os.path.isdir(src):
             shutil.copytree(src)
@@ -40,6 +67,20 @@ def copy(src, dest, options=None):
 
 
 def move(src, dest, options=None):
+    """Delete Function
+
+    Function allow deleting object by path. Work similar with file/dir/link.
+    If dir, move recursively.
+
+    Parameters:
+        src: object location.
+        dest: object destination location.
+        options: list of moving politics.
+
+    Returns:
+        value: 0 - successful, 1 - fail.
+
+    """
     try:
         shutil.move(src, dest)
         return 0
