92ac4b9868b55ba94a0d7be8edf370bb9a345e7b
Anton Krinitsyn <Kr1an@hotmail.com>
2017-03-31 20:02:22 +0300

    execution-manager/feature: add all current commands/docstring

--- utils/managers/execution_manager.py
+++ utils/managers/execution_manager.py
@@ -1,9 +1,54 @@
-from commands import delete_command, restore_command
+"""Execution Manager Module.
+
+Module uses commands module to execute user commands.
+Only accessible for outer scope function is 'execute_command', to which,
+You need to pass options, that describe which command it's needed to execute.
+
+Possible commands:
+* remove
+* restore
+
+Conversation with params:
+* if command is about restore/delete some files, than function should
+get 2 arguments: first one is a list of paths of regex path, second is option
+object with nessesary property mods(typeof list) that describe what manager
+should execute.
+* If path argument shows needed destination of location, this location should
+ be within 'path' property of 'options' object, but not in paths arguments.
+
+Example:
+    execute_command({'mods': ['restore', 'with_no_bin', ...]})
+
+Module is under construction. Could be extended.
+
+"""
+from utils.commands import delete_command, restore_command, bin_command
 
 
-def execute_command(paths, options):
+def execute_command(paths=[], options=None):
+    """Execute Command Function
+
+    Execute commands in place after argument_manager.
+
+    Args:
+       paths: possible list of given paths to delete or restore.
+       options: possible object with settings(mods) of needed command.
+
+    """
     if 'remove' in options['mods']:
         return delete_command.delete(paths, options)
 
     if 'restore' in options['mods']:
         return restore_command.restore(paths, options)
+
+    if 'empty_bin' in options['mods']:
+        return bin_command.empty_bin(options)
+
+    if 'create_bin' in options['mods']:
+        return bin_command.create_bin(options['path'], options)
+
+    if 'copy_bin' in options['mods']:
+        return bin_command.copy_bin(options['path'], options)
+
+    if 'move_bin' in options['mods']:
+        return bin_command.move_bin(options['path'], options)
