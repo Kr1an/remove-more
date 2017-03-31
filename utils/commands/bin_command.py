d73ab61526fc12cdfd50f5022a4f98c9978790e2
Anton Krinitsyn <Kr1an@hotmail.com>
2017-03-31 10:57:14 +0300

    bin-command-module/feature: add docstrings

--- utils/commands/bin_command.py
+++ utils/commands/bin_command.py
@@ -1,3 +1,15 @@
+"""Bin Command Module.
+
+Module offers possibility to work with bin structure.
+Copy, Move, Empty it.
+
+Example:
+    bin_command.move_bin(path, options)
+
+Todo:
+    * add option upgrade with different politics.
+    * write unittests
+"""
 import os
 
 from utils.helpers import clean_path
@@ -6,6 +18,18 @@ from utils.managers import user_config_manager
 
 
 def copy_bin(path, options):
+    """Copy Function
+
+    Function allow copy bin folder by path.
+
+    Parameters:
+        path: where to copy.
+        options: list of copy politics.
+
+    Returns:
+        value: 0 - successful, 1 - fail.
+
+    """
     try:
         clean_path.copy(user_config_manager.get_property('bin_path'), path)
         return 0
@@ -15,6 +39,19 @@ def copy_bin(path, options):
 
 
 def move_bin(path, options):
+    """Move Function
+
+    Function allow move bin folder by path with 
+    bin history changes.
+
+    Parameters:
+        path: where to move.
+        options: list of copy politics.
+
+    Returns:
+        value: 0 - successful, 1 - fail.
+
+    """
     try:
         clean_path.move(user_config_manager.get_property('bin_path'), path)
         user_config_manager.set_property('bin_path', path)
@@ -25,6 +62,19 @@ def move_bin(path, options):
 
 
 def create_bin(path, options):
+    """Create Function
+
+        Function allow to create bin in new location with empty bin.
+        Previous bin location is not deleter.
+
+        Parameters:
+            path: where to create.
+            options: list of copy politics.
+
+        Returns:
+            value: 0 - successful, 1 - fail.
+
+        """
     try:
         user_config_manager.set_property(
             'bin_path',
@@ -39,6 +89,17 @@ def create_bin(path, options):
 
 
 def empty_bin(options):
+    """Copy Function
+
+    Function allow to empty bin folder and history too.
+
+    Parameters:
+        options: list of copy politics.
+
+    Returns:
+        value: 0 - successful, 1 - fail.
+
+    """
     try:
         for history_item in bin_config_manager.get_property('history'):
             bin_config_manager.history_del(history_item['bin_name'])
