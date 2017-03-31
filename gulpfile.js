4478060c461991c672c7338b19f7bbfedf90f362
Anton Krinitsyn <Kr1an@hotmail.com>
2017-03-31 08:31:47 +0300

    task-runner/feature: add gulp to watch and backup files

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require("gulp"),
+    del = require('del'),
+    rename = require("gulp-rename"),
+    sourcemaps = require('gulp-sourcemaps'),
+    watch = require('gulp-watch');
+
+var pathToBackup = '../remove-more-backup';
+
+
+
+gulp.task('backup', function(){
+    return gulp.src('**/*')
+        .pipe(gulp.dest(pathToBackup))
+})
+gulp.task('watch', function() {
+  gulp.watch('**/*',
+  ['backup']);
+});
+gulp.task('default', ['backup'], function() {
+  gulp.start('watch');
+});
