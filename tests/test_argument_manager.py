4a19abcdf13248de47893f877f04eddd4f3a96af
Anton Krinitsyn <Kr1an@hotmail.com>
2017-04-02 00:39:26 +0300

    argument-manager/feature: add test initial structure

---
+++ tests/test_argument_manager.py
@@ -0,0 +1,22 @@
+"""Argument Manager Test Case.
+
+This module is a test case for module utils.managers.argument_manager.
+
+Example:
+    write 'python setup.py -t' in terminal.
+
+Test Module is not finished.
+
+"""
+import unittest
+import os
+import setup
+
+
+class ArgumentManagerTestCase(unittest.TestCase):
+    def setUp(self):
+        # use sys.args to add you args to default list: import sys
+        pass
+
+    def tearDown(self):
+        pass
